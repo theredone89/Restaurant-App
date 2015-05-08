02dae3ceccc477bfea6111240907fa42700dc288
The RedOne <theredone@192-168-0-104.rdsnet.ro>
2015-05-08 21:29:58 +0300

    changes to access management and added scan function test

--- www/js/index.js
+++ www/js/index.js
@@ -26,6 +26,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
+        document.getElementById('scan').addEventListener('click', this.scan, false);
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
@@ -55,5 +56,34 @@ var app = {
                 });
 
         console.log('Received Event: ' + id); 
+    },
+    
+    scan: function() {
+        console.log('scanning');
+        
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+        
+        scanner.scan( function (result) {
+                     
+                     alert("We got a barcode\n" +
+                           "Result: " + result.text + "\n" +
+                           "Format: " + result.format + "\n" +
+                           "Cancelled: " + result.cancelled);
+                     
+                     console.log("Scanner result: \n" +
+                                 "text: " + result.text + "\n" +
+                                 "format: " + result.format + "\n" +
+                                 "cancelled: " + result.cancelled + "\n");
+                     document.getElementById("info").innerHTML = result.text;
+                     console.log(result);
+                     /*
+                      if (args.format == "QR_CODE") {
+                      window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                      }
+                      */
+                     
+                     }, function (error) {
+                     console.log("Scanning failed: ", error);
+                     } );
     }
 };
